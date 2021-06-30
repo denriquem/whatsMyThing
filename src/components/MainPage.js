5937fcb2fd96a2a3c43e6efa5c71cf42928d5927
Daniel Martinez-Gatell <demartinezgatell@gmail.com>
2021-06-30 16:55:20 +0100

    initial setup

---
+++ src/components/MainPage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import WhatsMyThing from "./whatsMyThing";
+
+const MainPage = () => {
+	return <WhatsMyThing />;
+};
+
+export default MainPage;
---
+++ src/components/whatsMyThing.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const WhatsMyThing = () => {
+	return <h1>What's My Thing</h1>;
+};
+
+export default WhatsMyThing;
